541b1d19c8d3ed97d1c84b9d8bc6deaec54c31d9
karolis-kaz <karolis.kazakevicius@bazaarvoice.com>
2024-02-24 21:06:12 +0200

    Config: eslint, prettier, styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import fs from 'fs'; // Import fs module
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const globalsFilePath = join(__dirname, 'src', 'app', 'styles', 'variables.sass');
+
+let prependData = '';
+
+try {
+  // Read the content of globals.sass
+  const globalsContent = fs.readFileSync(globalsFilePath, 'utf-8');
+  prependData = `@import "${globalsFilePath}"\n${globalsContent}`;
+} catch (error) {
+  console.error(`Error reading ${globalsFilePath}: ${error.message}`);
+}
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [join(__dirname, 'styles')],
+    prependData,
+  }
+};
 
 export default nextConfig;
